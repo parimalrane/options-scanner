840df457d4b2ef1d4d22d48c86dcfc965660419b
parimalrane <parimalrane@gmail.com>
2026-08-06 08:46:01 +0530

    5th august data

--- test_diag.bat
+++
@@ -1,1 +0,0 @@
-python analyze_flow.py --active inputs\most-active-stock-options-08-04-2026.csv --decoi inputs\stocks-decrease-change-in-open-interest-08-04-2026.csv > diagnostic_test.txt
--- test_run.bat
+++
@@ -1,1 +0,0 @@
-call run_scan.bat 08-04-2026 > run_results.txt
