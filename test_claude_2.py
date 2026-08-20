d72252616b7604344e401a81da8d426ffadabedb
parimalrane <parimalrane@gmail.com>
2026-08-20 07:54:44 +0530

    19th august

--- test_claude_2.py
+++
@@ -1,7 +0,0 @@
-import traceback
-import subprocess
-try:
-    with open('test_results.txt', 'w') as f: f.write('starting\n')
-    subprocess.run(['taskkill', '/F', '/IM', 'python.exe'])
-except Exception as e:
-    with open('test_results.txt', 'w') as f: f.write(traceback.format_exc())
